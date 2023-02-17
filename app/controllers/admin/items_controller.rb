8daf2447ba95ffb4ec554dcd2cfa7f9bd360f6c0
Rinmal <121992801+Rinmal@users.noreply.github.com>
2023-02-17 04:39:12 +0000

    [Update] 商品追加機能

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,6 +3,17 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:notice] = "登録に成功しました"
+      redirect_to admin_item_path(@item)
+    else
+      render :index
+    end
   end
 
   def show
@@ -10,4 +21,11 @@ class Admin::ItemsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_sale)
+  end
+
 end
