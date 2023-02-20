f797664ccd556a5328dc0d4f5d163821563f5fcd
EC2 Default User <ec2-user@ip-172-31-3-220.ap-northeast-1.compute.internal>
2023-02-20 04:45:56 +0000

    update index show

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,8 +3,12 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
+    @orders= Order.all
+    
   end
 
   def show
+    @total= 'total_payment'+'postage'
+    @item_total= 'price'*'quantity'
   end
 end
