893de921ff5bed295162bf9e711f3586ab6a7e9b
EC2 Default User <ec2-user@ip-172-31-33-80.ap-northeast-1.compute.internal>
2023-02-20 07:48:30 +0000

    [Add]注文機能ほぼ実装

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,5 +2,5 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
   
-  enum production_status: { cannot_start: 0, wait_cooking: 1, cooking: 2, finished:  }
+  enum production_status: { cannot_start: 0, wait_cooking: 1, cooking: 2, finished: 3 }
 end
